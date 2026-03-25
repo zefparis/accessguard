f36c828c1a428eb8e38016ec3c56af25dda1d891
zefparis <ben.barere@gmail.com>
2026-03-26 00:04:45 +0100

    f

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import { InstallAppCard } from '../components/InstallAppCard'
 
 export function Home() {
   const nav = useNavigate()
@@ -47,6 +48,8 @@ export function Home() {
           <span key={t} className="badge badge-cyan">{t}</span>
         ))}
       </div>
+
+      <InstallAppCard appName="AccessGuard" badgeClassName="badge badge-cyan" />
     </div>
   )
 }
